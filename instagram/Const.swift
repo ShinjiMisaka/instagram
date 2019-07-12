794dbbb82e1bec219701f8f50b4469812f122c85
shinji.misaka <shinmisa0114@gmail.com>
2019-07-13 04:43:04 +0900

    タイムラインを実装した。

---
+++ instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  instagram
+//
+//  Created by 三坂真治 on 2019/07/13.
+//  Copyright © 2019 shinji.misaka. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
